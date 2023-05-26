36694147a35e40515f6452a3f01a96ed795377e7
jeferson.c.silva.glo <jeferson.c.silva@globalhitss.com.br>
2023-05-26 08:58:57 -0300

    Estilizando as tabs.

--- src/routes/app.routes.tsx
+++ src/routes/app.routes.tsx
@@ -2,9 +2,12 @@ import {
   createBottomTabNavigator,
   BottomTabNavigationProp,
 } from '@react-navigation/bottom-tabs';
+import { useTheme } from 'native-base';
 import { Home } from '@screens/Home';
 import { MyAds } from '@screens/MyAds';
 import { SignOut } from '@screens/SignOut';
+import { House, Tag, SignOut as SignOutIcon } from 'phosphor-react-native';
+import { Platform } from 'react-native';
 
 type AppRoutes = {
   home: undefined;
@@ -17,11 +20,48 @@ export type AppNavigatorRoutesProps = BottomTabNavigationProp<AppRoutes>;
 const { Navigator, Screen } = createBottomTabNavigator<AppRoutes>();
 
 export function AppRoutes() {
+  const { sizes, colors } = useTheme();
+
+  const iconSize = sizes[6];
   return (
-    <Navigator screenOptions={{ headerShown: false, tabBarShowLabel: false }}>
-      <Screen name="home" component={Home} />
-      <Screen name="myAds" component={MyAds} />
-      <Screen name="signOut" component={SignOut} />
+    <Navigator
+      screenOptions={{
+        headerShown: false,
+        tabBarShowLabel: false,
+        tabBarActiveTintColor: colors.gray[100],
+        tabBarInactiveTintColor: colors.gray[400],
+        tabBarStyle: {
+          backgroundColor: colors.gray[700],
+          borderTopWidth: 0,
+          height: Platform.OS === 'android' ? 'auto' : 96,
+          paddingBottom: sizes[10],
+          paddingTop: sizes[6],
+        },
+      }}
+    >
+      <Screen
+        name="home"
+        component={Home}
+        options={{
+          tabBarIcon: ({ color }) => <House color={color} size={iconSize} />,
+        }}
+      />
+      <Screen
+        name="myAds"
+        component={MyAds}
+        options={{
+          tabBarIcon: ({ color }) => <Tag color={color} size={iconSize} />,
+        }}
+      />
+      <Screen
+        name="signOut"
+        component={SignOut}
+        options={{
+          tabBarIcon: ({ color }) => (
+            <SignOutIcon color="#E07878" size={iconSize} />
+          ),
+        }}
+      />
     </Navigator>
   );
 }
