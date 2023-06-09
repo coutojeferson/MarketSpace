a050b3db5c06bc1d7afff39f87f8dad61cdf27ac
jeferson.c.silva.glo <jeferson.c.silva@globalhitss.com.br>
2023-06-09 12:00:39 -0300

    Criação de carousel de imagens utilizando flatList

--- src/screens/AdDetails.tsx
+++ src/screens/AdDetails.tsx
@@ -1,6 +1,6 @@
 import { Box, CheckIcon, HStack, Text, VStack } from 'native-base';
 import { Plus } from 'phosphor-react-native';
-import { Dimensions, TouchableOpacity } from 'react-native';
+import { Dimensions, TouchableOpacity, FlatList, Image } from 'react-native';
 import { Select } from 'native-base';
 import { useState } from 'react';
 import { CardItem } from '@components/CardItem';
@@ -11,6 +11,13 @@ export function AdDetails() {
   const [service, setService] = useState('Todos');
   const width = Dimensions.get('window').width;
 
+  const carouselData = [
+    { id: 1, source: require('../assets/item.png') },
+    { id: 2, source: require('../assets/item.png') },
+    { id: 3, source: require('../assets/item.png') },
+    // Adicione mais objetos para mais imagens
+  ];
+
   const navigation = useNavigation();
 
   function handleBack() {
@@ -20,6 +27,25 @@ export function AdDetails() {
   return (
     <VStack flex={1} px={6} mt={9}>
       <Header hideTitle onPress={handleBack} />
+      <Box mt={4}>
+        <FlatList
+          data={carouselData}
+          horizontal
+          pagingEnabled
+          keyExtractor={(item) => item.id.toString()}
+          renderItem={({ item }) => (
+            <Box>
+              <Image
+                source={item.source}
+                style={{
+                  width: Dimensions.get('window').width - 48,
+                  height: 200,
+                }}
+              />
+            </Box>
+          )}
+        />
+      </Box>
     </VStack>
   );
 }
